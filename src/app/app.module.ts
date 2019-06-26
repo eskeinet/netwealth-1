881e3eb116cc4d9d7cc5a822ff999bd67b7dfa0b
eskei <eskei@eskei.net>
2019-06-26 07:00:49 +0100

    - generate app ng new  --style=less - set up ngrx --install store npm install @ngrx/store --install tools npm install @ngrx/store-devtools --install effects install @ngrx/effects -- add empty state interface in the state folder -- initialize empty store for the main app -- initialize empty effects array for the main app -- initialize store devtools -check that all tests are running -commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'ESKEINET: sample app',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
